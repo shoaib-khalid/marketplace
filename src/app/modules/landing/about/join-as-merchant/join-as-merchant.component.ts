e0734adef81d985626d8df2c04c976236be7be02
Naqiudd33n <naquiddeen@kalsym.com>
2022-08-25 17:59:29 +0800

    additional fixes

--- src/app/modules/landing/about/join-as-merchant/join-as-merchant.component.ts
+++ src/app/modules/landing/about/join-as-merchant/join-as-merchant.component.ts
@@ -7,6 +7,8 @@ import { ChangeDetectorRef, Component, OnInit, ViewEncapsulation } from '@angula
 })
 export class JoinAsMerchantComponent
 {
+    marketplaceInfo: { phonenumber: string; email: string; address: string; reg:string };
+
     /**
      * Constructor
      */
@@ -16,7 +18,23 @@ export class JoinAsMerchantComponent
     }
 
     ngOnInit() {
-        console.log("ASsaasas");
-        
+        this.marketplaceInfo = {
+            email: "hello@deliverin.my",
+            phonenumber: "+60125033299",
+            address: "First Subang, Unit S-14-06, Level 14, Jalan SS15/4G, 47500 Subang Jaya, Selangor",
+            reg: "Symple Business System Sdn Bhd (SSM Reg #: 1436952-D)"
+        };
+    }
+
+    goToUrl(){
+        const phonenumber = this.marketplaceInfo.phonenumber.replace(/[^0-9]/g, '');
+        const message = encodeURI('Tell me more about joining Deliverin platform!')
+        window.open("https://wa.me/" + phonenumber + '?text=' + message, "_blank");
+
+        // this._document.location.href = "https://wa.me/" + phonenumber + '?text=' + message;
+    }
+    
+    joinUs() {
+        window.open("https://merchant.symplified.biz/sign-up")
     }
 }
