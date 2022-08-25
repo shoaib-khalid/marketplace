69556c708da2a033fbb62cbc556d39ec0c0c0570
Naqiudd33n <naquiddeen@kalsym.com>
2022-08-25 17:51:10 +0800

    create faq page

--- src/app/modules/landing/about/join-as-merchant/join-as-merchant.module.ts
+++ src/app/modules/landing/about/join-as-merchant/join-as-merchant.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'app/shared/shared.module';
 import { JoinAsMerchantComponent } from './join-as-merchant.component';
 import { joinAsMerchantRoutes } from './join-as-merchant.routing';
 
@@ -9,6 +13,10 @@ import { joinAsMerchantRoutes } from './join-as-merchant.routing';
     ],
     imports     : [
         RouterModule.forChild(joinAsMerchantRoutes),
+        MatButtonModule,
+        MatIconModule,
+        MatExpansionModule,
+        SharedModule
     ],
     bootstrap   : [
     ]
